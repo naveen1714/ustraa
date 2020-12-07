62f7b65e12352a0890e1f5bae305e2733fd072c2
naveensaini <naveensaini@policybazaar.com>
2020-12-07 15:53:19 +0530

    change for build

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -4,7 +4,6 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import StarIcon from "@material-ui/icons/Star";
 import "./../style/productList.css";
 import CardActions from "@material-ui/core/CardActions";
 import { Button } from "@material-ui/core";
